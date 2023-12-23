f8fbf1388bc1842667a543451152c3b4bef75d0c
julieta <chocobardiazjulieta13@gmail.com>
2023-12-23 16:31:57 -0300

    agregando img

--- src/componentes/vestidoCivil/vestidoCilvil.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const vestidoCilvil = () => {
-  return (
-    <div>vestidoCilvil</div>
-  )
-}
-
-export default vestidoCilvil
--- src/componentes/vestidoIglesia/vestidoIglesia.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const vestidoIglesia = () => {
-  return (
-    <div>vestidoIglesia</div>
-  )
-}
-
-export default vestidoIglesia
