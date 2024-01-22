db6b29a1df59b906c3efc41a361ca6ee0334c7bb
julieta <chocobardiazjulieta13@gmail.com>
2024-01-22 19:48:56 -0300

    cartWidget

--- src/componentes/CartWidget/CartWidget.jsx
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import './CarWidget.css';
+import { useContext } from 'react';
+import { CarritoContext } from '../../contex/carritoContex';
+import { Link } from 'react-router-dom';
+import './CartWidget.css';
 
 const CarWidget = () => {
   const imgCarrito = "https://i.pinimg.com/564x/cf/d8/cd/cfd8cddd259b44090f6cdaa4b502c9ee.jpg";
@@ -11,4 +14,4 @@ const CarWidget = () => {
   )
 }
 
-export default CarWidget
+export default CartWidget
