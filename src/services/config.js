cb1c67477bfe8e511b41f2627f34b2b919c1bf6c
julieta <chocobardiazjulieta13@gmail.com>
2024-01-23 15:49:28 -0300

    config

---
+++ src/services/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcCea01924p9z8u3IL-IILWW6dbonBET4",
+  authDomain: "vestidosonline-2360a.firebaseapp.com",
+  projectId: "vestidosonline-2360a",
+  storageBucket: "vestidosonline-2360a.appspot.com",
+  messagingSenderId: "154364978931",
+  appId: "1:154364978931:web:536e6707aaa1839ddb7986"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
